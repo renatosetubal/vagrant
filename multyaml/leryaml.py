5fa8bd09141dacc3dd778b615b77283b10823cdd
Renato Miranda Setúbal <remiset@gmail.com>
2024-08-28 12:54:50 -0300

    Update:Include Fedora 40 and conditional if will be ports in yaml file

--- multyaml/leryaml.py
+++ multyaml/leryaml.py
@@ -9,7 +9,8 @@ nome = dados[0]['disk']['size']
 # ['nome']
 # idade = dados['pessoa']['idade']
 # habilidades = dados['pessoa']['habilidades']
-
+nome = dados[0]['enableport']['enable']
 print(f"Nome: {nome}")
+
 # print(f"Idade: {idade}")
 # print(f"Habilidades: {', '.join(habilidades)}")
