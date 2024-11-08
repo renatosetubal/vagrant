13b1a0ea59e54eaaccd7d34b146f7d80510b917a
Renato Miranda Setúbal <remiset@gmail.com>
2024-11-08 15:43:25 -0300

    Update:Modificacao-inclusao maquina backup

--- Minio/scripts/MinioConnect.sh
+++ Minio/scripts/MinioConnect.sh
@@ -2,11 +2,12 @@
 
 # Defina as variáveis de configuração
 ALIAS="s3"                   # Nome do alias
-URL="http://192.168.2.10:9000"         # URL do servidor MinIO
-ACCESS_KEY="oln89bIGHVXGNBKQ2VVU"         # Chave de acesso
-SECRET_KEY="lSE4zAUVzhkp8tSs94FSgTFpbrKTFwg5PwFEr0pp"         # Chave secreta
-
+URL="http://10.0.19.41:9000"         # URL do servidor MinIO
+# ACCESS_KEY="oln89bIGHVXGNBKQ2VVU"         # Chave de acesso
+# SECRET_KEY="lSE4zAUVzhkp8tSs94FSgTFpbrKTFwg5PwFEr0pp"         # Chave secreta
+ACCESS_KEY="minioadmin"         # Chave de acesso
+SECRET_KEY="minioadmin"         # Chave secreta
 # Configura o alias no MinIO Client (mc)
 mc alias set "$ALIAS" "$URL" "$ACCESS_KEY" "$SECRET_KEY"
 
-#mc mirror --overwrite --remove s3 /mnt/lv_bkp
+#mc mirror --watch --overwrite --remove s3 /bkpminio
