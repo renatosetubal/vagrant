65a3b56b131907f080aaccc56de7e8e03bca1bac
Renato Miranda Setúbal <remiset@gmail.com>
2024-12-05 19:03:33 -0300

    Update: Alterado playbooks, feito nova estrutura de pastas para o ansible

--- Lab-Ansible/scripts/ansible.sh
+++ Lab-Ansible/scripts/ansible.sh
@@ -3,8 +3,12 @@ apt update;
 apt install -y net-tools software-properties-common ansible
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 cp -fv ~/.ssh/id_rsa.pub /vagrant/ansible_ssh.key
-echo "192.168.2.20 mariadb" >> /etc/hosts
-echo "192.168.2.30 apache" >> /etc/hosts
-echo "192.168.2.40 nginx" >> /etc/hosts
+cat <<EOT >> /etc/hosts
+192.168.2.20 mariadb
+192.168.2.30 apache
+192.168.2.40 nginx
+192.168.2.50 nfs
+192.168.2.60 java
+EOT
 echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> ~/.bashrc
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
